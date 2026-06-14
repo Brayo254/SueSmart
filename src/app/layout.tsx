fbbf30c56399e17d67e5a5f017ce5d999fefa323
Brian Itumo <b.itumo18@gmail.com>
2026-06-14 11:41:43 +0300

    Added made by section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { Toaster } from "@/components/ui/sonner";
 
+
 const playfair = Playfair_Display({
   variable: "--font-playfair",
   subsets: ["latin"],
---
+++ src/components/MadeBy.tsx
@@ -0,0 +1,18 @@
+"use client"
+import React from 'react'
+import Link from 'next/link'
+
+function MadeBy() {
+    return (
+        <div className='flex justify-center items-center'>
+
+            <Link href='https://wa.me/719316562'>
+
+                <p>Made with ❤️ by Brian: +254 719 316 562</p>
+
+            </Link>
+        </div>
+    )
+}
+
+export default MadeBy
